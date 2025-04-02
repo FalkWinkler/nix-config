e93bb91beba88977e68ef674233cd912bca2be17
Falk Winkler <fwinkler0573@gmail.com>
2025-04-02 07:22:41 +0200

    firewall hinzugefügt

---
+++ system/hosts/firewall/default.nix
@@ -0,0 +1,37 @@
+{ ... }:
+{
+  imports = [
+    ./disk-config.nix
+    ./hardware-configuration.nix
+    ./_modules # host specific modules
+    ../../profiles/server.nix
+  ];
+
+  config = {
+    myHardware.cpu = "intel";
+
+    mySystem = {
+      programs.nh = {
+        enable = true;
+        flake = "/home/budiman/Github/nix-config";
+      };
+      services = {
+        btrfs-autoscrub = {
+          enable = true;
+          fileSystems = [ "/" ];
+        };
+        restic-backup.enable = true;
+      };
+    };
+
+    services.openssh.listenAddresses = [
+      { addr = "10.0.0.114"; }  
+    ];
+
+    # sshd failed to start on boot when interface is not ready yet
+    systemd.services.sshd = {
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
+    };
+  };
+}
