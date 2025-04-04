c86a572ab23b40d534f23c95ba13e1f5bbf2220d
Falk Winkler <fwinkler0573@gmail.com>
2025-04-04 04:23:00 +0200

    firewall disk angepasst

--- system/hosts/firewall/hardware-configuration.nix
+++ system/hosts/firewall/hardware-configuration.nix
@@ -15,20 +15,20 @@
     kernelModules = [ "kvm-intel" ];  
   };
 
-  fileSystems = {
-    "/boot" = {
-      device = "/dev/disk/by-uuid/C241-FDB6";
-      fsType = "vfat";
-      options = [
-        "fmask=0022"
-        "dmask=0022"
-      ];
-    };
-    "/" = {
-      device = "/dev/disk/by-uuid/c85a6137-9a10-4e79-a5a5-86368db22114";
-      fsType = "ext4";
-    };
-  };
+  # fileSystems = {
+  #   "/boot" = {
+  #     device = "/dev/disk/by-uuid/C241-FDB6";
+  #     fsType = "vfat";
+  #     options = [
+  #       "fmask=0022"
+  #       "dmask=0022"
+  #     ];
+  #   };
+  #   "/" = {
+  #     device = "/dev/disk/by-uuid/c85a6137-9a10-4e79-a5a5-86368db22114";
+  #     fsType = "ext4";
+  #   };
+  # };
 
-  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
